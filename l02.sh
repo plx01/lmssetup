ff6f7934cab7bf8b0b79720bc5089b9fff0be86b
plx01 <58028825+plx01@users.noreply.github.com>
2021-06-12 14:30:52 +0700

    Create l02.sh

---
+++ l02.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#-------------------------
+# 5 Install Canvas
+# 1
+sudo apt-get install git-core
+# 2
+git clone https://github.com/instructure/canvas-lms.git ~/canvas
+# 3
+cd ~/canvas  && git checkout stable
+# 4-5
+sudo mkdir -p /var/canvas  && sudo chown -R $USER /var/canvas
+sudo cp -R ~/canvas /var  && cd /var/canvas
+# 6
+bundle config set path 'vendor/bundle'
+bundle config set --local without 'pulsar'
