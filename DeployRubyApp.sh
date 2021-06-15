c9685ac04cc5a43f2d396e0de5f41525ff5a8179
plx01 <58028825+plx01@users.noreply.github.com>
2021-06-15 09:48:04 +0700

    Update DeployRubyApp.sh

--- DeployRubyApp.sh
+++ DeployRubyApp.sh
@@ -32,11 +32,15 @@ sudo usermod -a -G sudo myappuser
 # 2 Preparing the app's environment
 
 # 2.1 Login as the app's user
+sudo usermod -a -G rvm myappuser
 sudo -u myappuser -H bash -l
 # source /etc/profile.d/rvm.sh
 # rvm use ruby-2.5.1
-# rvm insatall ruby-2.5.0  && rvm --default use ruby-2.5.0
-gem install json -v '1.8.2' --source 'https://rubygems.org/'
+# rvm install ruby-2.5.0  && rvm --default use ruby-2.5.0
+# rvm install ruby-2.5.1  && rvm --default use ruby-2.5.1
+# gem install json -v '1.8.2' --source 'https://rubygems.org/'
+# gem install rake -v '10.4.2' --source 'https://rubygems.org/'
+bundle update json
 exit
 
 # 2.2 Install app dependencies
@@ -46,3 +50,32 @@ cd /var/www/myapp/code &&
 bundle install
 # --path ./vendor/bundle/
 # --deployment --without development test
+
+# 2.3 Configure database.yml and secrets.yml
+nano config/database.yml
+# bundle exec rake secret
+nano config/secrets.yml
+chmod 700 config db && 
+chmod 600 config/database.yml config/secrets.yml
+
+# 2.4 Compile Rails assets and run database migrations
+bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+
+# 3 Configuring Apache and Passenger
+# 3.1 Determine the Ruby command that Passenger should use
+passenger-config about ruby-command
+# Out: ommand: /usr/local/rvm/gems/ruby-2.5.1/wrappers/ruby
+
+# 3.2 Go back to the admin account
+exit
+
+# 3.3 Edit Apache configuration file
+# sudo nano /etc/apache2/sites-enabled/myapp.conf
+# sudo nano /etc/httpd/conf.d/myapp.conf
+git clone https://github.com/plx01/lmssetup ~/02l
+sudo cp ~/02l/myapp.conf /etc/apache2/sites-enabled/myapp.conf
+sudo service apache2 restart && sudo service apache restart
+
+# 3.4 Test drive
+curl http://phucenter.net/
+
