2a81e4a7fe9950a8acac64ceaae8893d38cde173
plx01 <58028825+plx01@users.noreply.github.com>
2021-06-15 08:03:36 +0700

    Update DeployRubyApp.sh

--- DeployRubyApp.sh
+++ DeployRubyApp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Deploying a Ruby app on Digital Ocean production server
+# git clone https://github.com/plx01/lmssetup ~/01l
+# sudo cp ~/01l/DeployRubyApp.sh ~/dra.sh
+# sudo chmod +x ~/dra.sh  && sudo ~/dra.sh
 
 # 1 Transferring the app code to the server
 
@@ -24,22 +27,22 @@ sudo mkdir -p /var/www/myapp  &&
 sudo chown myappuser: /var/www/myapp
 cd /var/www/myapp  && 
 sudo -u myappuser -H git clone --branch=end_result https://github.com/phusion/passenger-ruby-rails-demo.git code
+sudo usermod -a -G sudo myappuser
 
 # 2 Preparing the app's environment
 
 # 2.1 Login as the app's user
 sudo -u myappuser -H bash -l
 # source /etc/profile.d/rvm.sh
-rvm use ruby-2.5.1
+# rvm use ruby-2.5.1
+# rvm insatall ruby-2.5.0  && rvm --default use ruby-2.5.0
+gem install json -v '1.8.2' --source 'https://rubygems.org/'
+exit
 
 # 2.2 Install app dependencies
-exit
-rvm insatall ruby-2.5.0
-rvm --default use ruby-2.5.0
-gem install json -v '1.8.2' --source 'https://rubygems.org/'
 sudo chown myappuser: /var/www/myapp
 sudo -u myappuser -H bash -l
-cd /var/www/myapp/code  && 
+cd /var/www/myapp/code && 
 bundle install
 # --path ./vendor/bundle/
 # --deployment --without development test
